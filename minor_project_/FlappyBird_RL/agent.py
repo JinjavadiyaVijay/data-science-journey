608621f0d5e59c9b44a3b83fa2c4dea68f4747d3
Vijayjinjavadiya <jinjuvadiyavijay56@gmail.com>
2026-06-04 19:56:27 +0530

    "feat: implement ReplayMemory with deque and initialize DQN network"

--- minor_project_/FlappyBird_RL/agent.py
+++ minor_project_/FlappyBird_RL/agent.py
@@ -1,18 +1,39 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
-env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=True)
+from DQN import dqn
+from experience_replay import ReplayMemory
 
-state, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
+# detect gpu and set gpu 
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
 
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
+
+def run(self,is_training = True,render = False):
+    env = gym.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=True)
     
-    # Checking if the player is still alive
-    if terminated:
-        break
+    num_state = env.observation_space.shape[0] # input dim
+    num_actions = env.actions_space.n # output dim
 
-env.close()
+    policy_dqn = dqn(num_state,num_actions).to(device)
+    state, _ = env.reset()
+
+    if is_training:
+        memory = ReplayMemory(10000)
+
+    while True:
+        # Next action:
+        # (feed the observation to your agent here)
+        action = env.action_space.sample()
+    
+        # Processing:
+        next_state, reward, terminated, _, _ = env.step(action)
+        
+        if is_training:
+            memory.append((state, action, new_state, reward, terminated))
+        
+        if terminated:
+            break
+    
+    env.close()
---
+++ minor_project_/FlappyBird_RL/experience_replay.py
@@ -0,0 +1,17 @@
+from collections import deque
+import random
+
+class ReplayMemory():
+    # create FIFO queue
+    def __init__(self, max_len, seed=None):
+        self.memory = deque([],max_len = max_len)
+    
+    def append(self, new_exp):
+        self.memory.append(new_exp)
+
+    def sample(self, sample_size):
+        return random.sample(self.memory, sample_size)
+    
+    # curr buffer size
+    def __len__(self):
+        return len(self.memory)
