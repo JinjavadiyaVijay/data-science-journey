ad51a283f48a05008e139617cc254893d0517499
Vijayjinjavadiya <jinjuvadiyavijay56@gmail.com>
2026-07-17 18:32:10 +0530

    integreted diffrent tools

--- Deep_Learing/AgenticAI/agent.py
+++ Deep_Learing/AgenticAI/agent.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from agno.agent import Agent
 from agno.tools.workspace import Workspace
 from agno.models.groq import Groq
+from agno.tools.duckduckgo import DuckDuckGoTools 
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,10 +10,14 @@ load_dotenv()
 def build_agent():
     return Agent(
         model = Groq(id="qwen/qwen3-32b"),
-        instructions ="you are famous stand-up comdain",
+        tools= [DuckDuckGoTools()],
+        #instructions ="you are famous stand-up comdain",
         markdown = True,
+        add_datetime_to_context=True,
+
+
 
     )
 
 agent = build_agent()
-agent.print_response("tell me dark double meaning jokes")
+agent.print_response("wht is starting CTC for recently pass out diploma student with some good project and skills ")
